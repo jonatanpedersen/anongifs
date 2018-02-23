1959284ef9cba18f7a8ec8e089184ac88264f930
Jonatan Pedersen <jp@jonatanpedersen.com>
2018-02-23 13:32:17 +0100

    done

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
-
+const glob = require('glob');
 const app = express();
 
-app.get('/:id', (req, res) => {
-	const {id} = req.params;
-	res.render('index', {id});
+app.set('view engine', 'pug');
+
+app.use('/gifs', express.static('gifs'));
+
+app.get('/', (req, res) => {
+	const gifs = getGifs();
+	res.render('index', {gifs});
+});
+
+app.get('/random', (req, res) => {
+	const gifs = getGifs();
+	var gif = gifs[Math.floor(Math.random()*gifs.length)];
+
+	res.render('details', {gif});
+});
+
+app.get('/:gif', (req, res) => {
+	const {gif} = req.params;
+	res.render('details', {gif});
 });
 
 const port = process.env.PORT || 1030;
@@ -12,3 +28,7 @@ const port = process.env.PORT || 1030;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
+
+function getGifs () {
+	return glob.sync('*.gif', {cwd: './gifs/'});
+}
